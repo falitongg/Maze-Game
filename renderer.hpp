568d14492f20f3fa8a337d14e9afb77795a372ae
AntonSokolov <falitongg@gmail.com>
2025-12-24 22:13:02 +0100

    Add screen cleaner

--- renderer.hpp
+++ renderer.hpp
@@ -4,10 +4,23 @@
 
 #ifndef CPPSEMESTRALPRJCT_RENDERER_HPP
 #define CPPSEMESTRALPRJCT_RENDERER_HPP
+#include <cstdlib>
+
 #include "maze.hpp"
 #include "player.hpp"
 
 class Renderer {
+    private:
+        /**
+         * @brief Clears screen
+         */
+        void clearScreen() const {
+            #ifdef _WIN32
+            std::system("cls");
+            #else
+                system("clear");
+            #endif
+        }
     public:
         Renderer();
         void draw(const Maze& maze, const Player& player) const;
