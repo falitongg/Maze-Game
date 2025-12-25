61a80d3fdf4ef1abb466bda4208d5c90cbc16023
AntonSokolov <falitongg@gmail.com>
2025-12-25 22:13:19 +0100

    Add program argument '--help'

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include "game.hpp"
+#include "helper.hpp"
+#include <iostream>
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc > 1) {
+        std::string arg = argv[1];
+        if (arg == "--help") {
+            printHelp();
+            return 0;
+        }
+    }
     Game game;
     game.initialize();
     game.run();
