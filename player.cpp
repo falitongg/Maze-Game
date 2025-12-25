7022509f7d7652577ffaa4e17d4d41789f2089c8
AntonSokolov <falitongg@gmail.com>
2025-12-26 00:10:48 +0100

    Add reading of capital letters

--- player.cpp
+++ player.cpp
@@ -23,12 +23,6 @@ std::pair<int, int> Player::getCurrentPosition() const {
     return currentPosition;
 }
 
-void Player::setX(int x) {
-}
-
-void Player::setY(int y) {
-}
-
 void Player::setCurrentPosition(int x, int y) {
     currentPosition = std::make_pair(x, y);
 }
@@ -37,10 +31,10 @@ void Player::go(char direction, const Maze& maze) {
     int newX = getX();
     int newY = getY();
     switch (direction) {
-        case 'w': newY = getY() - 1; break;
-        case 's': newY = getY() + 1; break;
-        case 'a': newX = getX() - 1; break;
-        case 'd': newX = getX() + 1; break;
+        case 'w': case 'W': newY = getY() - 1; break;
+        case 's': case 'S': newY = getY() + 1; break;
+        case 'a': case 'A': newX = getX() - 1; break;
+        case 'd': case 'D': newX = getX() + 1; break;
         default: return;
     }
     if (maze.isInMazeBounds(newX, newY) && !maze.isWall(newX, newY)) {
