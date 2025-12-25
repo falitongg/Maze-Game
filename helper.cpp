d7ff6b9ff3cc82aa46ebdecb5d4076128ffe97d2
AntonSokolov <falitongg@gmail.com>
2025-12-25 22:12:58 +0100

    Realization helper.hpp

---
+++ helper.cpp
@@ -0,0 +1,13 @@
+//
+// Created by антін on 25/12/2025.
+//
+
+#include "helper.hpp"
+
+#include <iostream>
+
+void printHelp() {
+    std::cout << "Player's position: '@'\n";
+    std::cout << "Controls: W/A/S/D - move, Q - quit, help - 'H'\n";
+    std::cout << "Reach the exit 'E' to win!\n\n";
+}
