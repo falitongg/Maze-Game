26ca6c618588f77245f0bf8dbc0ea0c213c13c5e
AntonSokolov <falitongg@gmail.com>
2025-12-26 15:18:14 +0100

    Add maze size selection menu and custom size selection validation

--- game.cpp
+++ game.cpp
@@ -10,8 +10,84 @@
 Game::Game() : isRunning(false) {
 }
 
+char helloFunction() {
+    std::cout << "Welcome to the game!\n";
+    std::cout << "Choose preferred maze size :\n [1] - SMALL(31x15)\n [2] - MEDIUM(51x21)\n [3] - LARGE(101x21)\n [4] - custom size \n";
+    std::cout << "Press H to show tutorial\n";
+    std::cout << "Press Enter to continue...\n";
+    char input = std::cin.get();
+    while (input != '1' && input != '2' && input != '3' && input != '4') {
+        std::cout << "CHOOSE IT NOW\n";
+        input = std::cin.get();
+    }
+    return input;
+}
+
+std::pair<int, int> customChoice() {
+    std::cin.clear();
+
+    int width = 0;
+    int height = 0;
+
+    while (true) {
+        std::cout << "Enter maze width (11-99): ";
+        std::cin >> width;
+
+        std::cout << "Enter maze height (11-99): ";
+        std::cin >> height;
+
+        bool tooWide  = width  >= 100;
+        bool tooHigh  = height >= 100;
+        bool hugeSquare = (width == height && width >= 60);
+
+        if (std::cin.fail()) {
+            // if user didnt enter int
+            std::cin.clear();
+            std::cin.ignore(10000, '\n');  // clears garbage
+            std::cout << "Invalid input. Please enter positive integers.\n";
+            continue;
+        }
+
+        if (tooWide || tooHigh || hugeSquare) {
+            std::cout << "Too large size. Try smaller values.\n";
+            continue;
+        }
+        if (width < 11 || height < 11) {
+            std::cout << "Width and height must be at least 11. Try again.\n";
+            continue;
+        }
+
+        break;
+    }
+
+    if (width % 2 == 0) {
+        width += 1;
+    }
+    if (height % 2 == 0) {
+        height += 1;
+    }
+
+    return {width, height};
+}
+
 void Game::initialize() {
-    maze = Maze(91,21);
+    int width = 0;
+    int height = 0;
+
+    char choice = helloFunction();
+    switch (choice) {
+        case '1': width = 31; height = 15; break;
+        case '2': width = 51; height = 21; break;
+        case '3': width = 101; height = 21; break;
+        case '4':{
+            auto preferredSize = customChoice();
+            width = preferredSize.first;
+            height = preferredSize.second;
+            break;
+        };
+    }
+
+    maze = Maze(width,height);
     generator = Generator();
 
     // for (int i = 1; i < 9; i++) {
